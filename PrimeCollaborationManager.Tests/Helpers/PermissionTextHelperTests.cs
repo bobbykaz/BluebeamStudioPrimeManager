bdf4e92c53fbaab239e2a713c41b1d47397a305b
bobby kaz <robert.kazimiroff@gmail.com>
2020-06-03 22:12:04 -0700

    Permission Text display updated

---
+++ PrimeCollaborationManager.Tests/Helpers/PermissionTextHelperTests.cs
@@ -0,0 +1,37 @@
+﻿using Xunit;
+using PrimeCollaborationManager.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PrimeCollaborationManager.Helpers.Tests
+{
+    public class PermissionTextHelperTests
+    {
+        [Theory]
+        [InlineData("","")]
+        [InlineData("asdf", "asdf")]
+        [InlineData("SaveCopy", "Save As")]
+        [InlineData("PrintCopy", "Print")]
+        [InlineData("Markup", "Markup")]
+        [InlineData("AddDocuments", "Add Documents")]
+        [InlineData("MarkupAlert", "Markup Alert")]
+        [InlineData("UndoCheckouts", "Revoke Check Out")]
+        [InlineData("CreateSessions", "Send PDFs to Studio Sessions")]
+        [InlineData("ShareItems", "Share File Links")]
+        [InlineData("Invite", "Send Invitations")]
+        [InlineData("ManageParticipants", "Manage User Access")]
+        [InlineData("ManagePermissions", "Manage Permissions")]
+        [InlineData("FullControl", "Full Control")]
+        public void GetPermissionDisplayNameTest(string input, string output)
+        {
+            Assert.Equal(output, PermissionTextHelper.GetPermissionDisplayName(input));
+        }
+
+        [Fact]
+        public void GetPermissionDisplayName_NullTest()
+        {
+            Assert.Throws<ArgumentNullException>(() => PermissionTextHelper.GetPermissionDisplayName(null));
+        }
+    }
+}
---
+++ PrimeCollaborationManager/Helpers/PermissionTextHelper.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrimeCollaborationManager.Helpers
+{
+    public static class PermissionTextHelper
+    {
+        public static string GetPermissionDisplayName(string permType)
+        {
+            if (permType == null)
+                throw new ArgumentNullException(nameof(permType));
+
+            switch (permType)
+            {
+                case "SaveCopy":
+                    return "Save As";
+                case "PrintCopy":
+                    return "Print";
+                case "Markup":
+                    return "Markup";
+                case "AddDocuments":
+                    return "Add Documents";
+                case "MarkupAlert":
+                    return "Markup Alert";
+                case "UndoCheckouts":
+                    return "Revoke Check Out";
+                case "CreateSessions":
+                    return "Send PDFs to Studio Sessions";
+                case "ShareItems":
+                    return "Share File Links";
+                case "Invite":
+                    return "Send Invitations";
+                case "ManageParticipants":
+                    return "Manage User Access";
+                case "ManagePermissions":
+                    return "Manage Permissions";
+                case "FullControl":
+                    return "Full Control";
+                default:
+                    return permType;
+            }
+        }
+    }
+}
