382b5f5ed8c5bf4ae06cccaf8f07937b5eda535a
Bobby Kazimiroff <robert.kazimiroff@gmail.com>
2021-05-05 19:56:09 -0700

    Removing build scripts

--- push.ps1
+++
@@ -1,8 +0,0 @@
-$today =  Get-Date -Format "yy.MM.dd"
-$today = "0.1." + $today
-
-$ecrTag = "591069284328.dkr.ecr.us-west-1.amazonaws.com/bk-bbprime-mgr:" + $today
-
-aws ecr get-login-password --region us-west-1 | docker login --username AWS --password-stdin 591069284328.dkr.ecr.us-west-1.amazonaws.com
-
-docker push $ecrTag
